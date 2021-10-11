26d45e39283cfda00eab37aa883c7be224630f6a
Stephanie J. Spielman <spielman@rowan.edu>
2021-10-11 09:03:43 -0400

    paths activity

--- R/launch_activity.R
+++ R/launch_activity.R
@@ -18,7 +18,7 @@ If you think are getting this message in error, you might have a typo or used th
 #' @export
 launch_activity <- function(choice = NULL)
 {
-  allowed <- c("rmarkdown")
+  allowed <- c("rmarkdown","paths")
   if (is.null(choice))
   {
     activity_choice_message()
@@ -35,6 +35,7 @@ launch_activity <- function(choice = NULL)
     # Define
     activity_name <- dplyr::case_when(
       choice == "rmarkdown" ~ "demonstration_rmarkdown.Rmd",
+      choice == "paths" ~ "mammals_activity.zip"
       TRUE ~ ""
     )
     if(activity_name == "") stop()
@@ -46,18 +47,17 @@ launch_activity <- function(choice = NULL)
       "https://raw.githubusercontent.com/sjspielman/datascience_for_biologists/master/docs/inclass/{activity_name}"
     )
     
-    # If HW path and HW exist, just open the HW
     if (dir.exists(final_activity_path)) {
       message(
         glue::glue(
           stringr::str_wrap(
-            "\nIt looks like you have already launched this activity. I'll just open it for you.", width = 60)
+            "\nIt looks like you have already launched this activity.", width = 60)
         )
       )
-      open_thing(final_activity_path)
+      #open_thing(final_activity_path)
     } else {
       download_thing(activity_path, activity_name, activity_raw_url)
-      open_thing(final_activity_path)
+      #open_thing(final_activity_path)
     }
     return (invisible(choice))
   }
